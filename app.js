5fbed9f74b7a097401fcdaa4f1f05a58a84feb3e
Julie Chen <juliechenmedicine@gmail.com>
2024-08-31 21:26:17 -0700

    routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
 const { PORT = 3001 } = process.env;
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+app.use("/", indexRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
