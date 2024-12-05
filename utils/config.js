91add15c9e7665098b790b731f09d56933e1d9f7
Julie Chen <juliechenmedicine@gmail.com>
2024-12-05 12:19:41 -0800

    local

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,9 @@
 const crypto = require("crypto");
 
-const JWT_SECRET = crypto.randomBytes(32).toString("hex");
-module.exports.JWT_SECRET = JWT_SECRET;
+// const JWT_SECRET = crypto.randomBytes(32).toString("hex");
+// module.exports.JWT_SECRET = JWT_SECRET;
+const { JWT_SECRET = "super-strong-secret" } = process.env;
+
+module.exports = {
+  JWT_SECRET,
+};
