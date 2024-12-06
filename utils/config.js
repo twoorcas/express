31f189926b57a965483af4dc955c0cac1dcb2a45
Julie Chen <juliechenmedicine@gmail.com>
2024-12-06 11:12:27 -0800

    endpoint

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,3 @@
-const crypto = require("crypto");
-
 const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = {
--- utils/errorclass/ValidationError.js
+++ utils/errorclass/ValidationError.js
@@ -7,4 +7,4 @@ class ValidationError extends Error {
 }
 
 module.exports.ValidationError = ValidationError;
-//bad request
+// bad request
